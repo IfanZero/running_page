9c42ec0cc88b605ad1faf712425bf68f181c1466
ifanzero <linyangfan940403@126.com>
2026-01-04 15:16:06 +0800

    修正代码

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -233,7 +233,6 @@ export const MAP_TILE_STYLE_LIGHT = 'light-v10';
 export const MAP_TILE_STYLE_DARK = 'dataviz-dark';
 export const MAP_TILE_STYLE = 'dataviz-dark';
 
-
 // access token. you can apply a new one, it's free.
 // maptiler: Gt5R0jT8tuIYxW6sNrAg | sign up at https://cloud.maptiler.com/auth/widget
 // stadiamaps: 8a769c5a-9125-4936-bdcf-a6b90cb5d0a4 | sign up at https://client.stadiamaps.com/signup/
